fb9e908e6569622f00104f95be8a3d3231ca0d1b
xueyj <1203378176@qq.com>
2026-05-15 22:42:54 +0800

    第一次进行数据的提交。。

--- TestModue01/src/com/mashibing/Student.java
+++ TestModue01/src/com/mashibing/Student.java
@@ -1,10 +1,10 @@
 package com.mashibing;
 
 /**
- * @Author:咕咕嘎嘎
- * @Date:2026/5/15 - 05 - 15
- * @Description:com.mashibing
- * @version:21
+ * @author 咕咕嘎嘎
+ * @since 2026/5/15
+ * Description: com.mashibing
+ * @version 21
  */
 public class Student {
 }
